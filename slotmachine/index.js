379054e88c74b50159a3e11beff96d0feb4d58b1
BenBiggen <benjamin.storesund@gmail.com>
2025-11-11 21:42:20 +0100

    oktober 11 kveld

--- slotmachine/index.js
+++ slotmachine/index.js
@@ -1,8 +1,19 @@
-const jackpot = ["7", "J", "!", "6"]
-let screen1
-let screen2
-let screen3
+const jackpot = ["7", "J", "!", "6"];
+let screen1;
+let screen2;
+let screen3;
+const button = document.getElementById("gambleButton");
 
-document.getElementById("screen1").innerText=screen1
-document.getElementById("screen2").innerText=screen2
-document.getElementById("screen3").innerText=screen3
+button.addEventListener("click", () => {
+  trekk();
+});
+
+function trekk() {
+    screen1 = jackpot[Math.floor(Math.random() * jackpot.length)]
+    screen2 = jackpot[Math.floor(Math.random() * jackpot.length)]
+    screen3 = jackpot[Math.floor(Math.random() * jackpot.length)]
+    document.getElementById("screen1").innerText=screen1
+    document.getElementById("screen2").innerText=screen2
+    document.getElementById("screen3").innerText=screen3
+};
+
