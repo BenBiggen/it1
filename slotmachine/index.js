da0610012b8c570d44ff72e7b647d0e3c39bd715
BenBiggen <benjamin.storesund@gmail.com>
2025-11-11 15:49:08 +0100

    11 oktober

---
+++ slotmachine/index.js
@@ -0,0 +1,8 @@
+const jackpot = ["7", "J", "!", "6"]
+let screen1
+let screen2
+let screen3
+
+document.getElementById("screen1").innerText=screen1
+document.getElementById("screen2").innerText=screen2
+document.getElementById("screen3").innerText=screen3
