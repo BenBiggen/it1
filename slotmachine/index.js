9a9a0d6fbb7ff76a2dfedadfbdf5728b3709f1ea
BenBiggen <benjamin.storesund@gmail.com>
2025-11-25 14:49:22 +0100

    25 November

--- slotmachine/index.js
+++ slotmachine/index.js
@@ -7,6 +7,7 @@ const grapeJack = ["🍉", "🍋", "🍋", "🍒", "🍒", "🍍", "🍌", "🍋
 const pineappleJack = ["🍍", "🍍", "🍍", "🍍", "🍍", "🍍", "🍉", "🍋", "🍒", "🍒", "🍇", "🍌", "🍋"];
 const bananaJack = ["🍉", "🍋"];
 const button = document.getElementById("gambleButton");
+const gambleButtonColor = document.getElementById("gambleButton");
 let trekkAntall = 0;
 let limeJackpots = 0;
 let melonJackpots = 0;
@@ -15,6 +16,11 @@ let cherryJackpots = 0;
 let grapeJackpots = 0;
 let pineJackpots = 0;
 let bananaJackpots = 0;
+let spinning = false;
+const screens = {
+s1: document.getElementById("screen1"),
+s2: document.getElementById("screen2"),
+s3: document.getElementById("screen3")}
 
 button.addEventListener("click", () => {
   trekk();
@@ -25,87 +31,95 @@ function delay(ms) {
 }
 
 async function trekk() {
-  let array;
-  trekkAntall++;
-  document.getElementById("trekkAntall").innerText="du har gamblet "+trekkAntall+" gang(er)";
-  let screen1 = "";
-  let screen2 = "";
-  let screen3 = "";
-  document.getElementById("screen1").innerText=screen1;
-  document.getElementById("screen2").innerText=screen2;
-  document.getElementById("screen3").innerText=screen3;
-
-    for (let i = 0; i < 8; i++) {
-      screen1 = jackpot[Math.floor(Math.random() * jackpot.length)]
-      document.getElementById("screen1").innerText=screen1
-      await delay(200);
-    }
-
-    if (screen1==="🍋‍🟩") {
-      array = jackpot.concat(limeJack);
-    }
-    if (screen1==="🍉") {
-      array = jackpot.concat(melonJack);
-    }
-    if (screen1==="🍋") {
-      array = jackpot.concat(lemonJack);
-    }
-    if (screen1==="🍒") {
-      array = jackpot.concat(cherryJack);
-    }
-    if (screen1==="🍇") {
-      array = jackpot.concat(grapeJack);
-    }
-    if (screen1==="🍍") {
-      array = jackpot.concat(pineappleJack);
-    }
-    if (screen1==="🍌") {
-      array = jackpot.concat(bananaJack);
-    }
+  if (spinning === false) {
+    spinning = true;
+    gambleButtonColor.style.backgroundColor = "grey";
+    let array;
+    trekkAntall++;
+    document.getElementById("trekkAntall").innerText="du har gamblet "+trekkAntall+" gang(er)";
+    let screen1 = "";
+    let screen2 = "";
+    let screen3 = "";
+    screens.s1.innerText=screen1;
+    screens.s2.innerText=screen2;
+    screens.s3.innerText=screen3;
 
       for (let i = 0; i < 8; i++) {
-      screen2 = array[Math.floor(Math.random() * array.length)];
-      document.getElementById("screen2").innerText=screen2;
-      await delay(200);
-    }
-
-      for (let i = 0; i < 8; i++) {
-      screen3 = array[Math.floor(Math.random() * array.length)];
-      document.getElementById("screen3").innerText=screen3;
-      await delay(200);
-    }
-
-    if (screen1 === screen2 && screen2 === screen3) {
-      console.log(`Du har vunnet ${screen1} jackpot`);
-      document.getElementById("jackpotPrint").innerText=(`Du har vunnet ${screen1} jackpot`);
+        screen1 = jackpot[Math.floor(Math.random() * jackpot.length)]
+        screens.s1.innerText=screen1
+        await delay(200);
+      }
 
       if (screen1==="🍋‍🟩") {
-        limeJackpots++;
-        document.getElementById("limeJackpots").innerText="du har vunnet 🍋‍🟩 jackpot: " + limeJackpots + " gang(er)";
+        array = jackpot.concat(limeJack);
       }
       if (screen1==="🍉") {
-        melonJackpots++;
-        document.getElementById("melonJackpots").innerText="du har vunnet 🍉 jackpot: " + melonJackpots + " gang(er)";
+        array = jackpot.concat(melonJack);
       }
       if (screen1==="🍋") {
-        lemonJackpots++;
-        document.getElementById("lemonJackpots").innerText="du har vunnet 🍋 jackpot: " + lemonJackpots + " gang(er)";
+        array = jackpot.concat(lemonJack);
       }
       if (screen1==="🍒") {
-        cherryJackpots++;
-        document.getElementById("cherryJackpots").innerText="du har vunnet 🍒 jackpot: " + cherryJackpots + " gang(er)";
+        array = jackpot.concat(cherryJack);
       }
       if (screen1==="🍇") {
-        grapeJackpots++;
-        document.getElementById("grapeJackpots").innerText="du har vunnet 🍇 jackpot: " + grapeJackpots + " gang(er)";
+        array = jackpot.concat(grapeJack);
       }
       if (screen1==="🍍") {
-        pineJackpots++;
-        document.getElementById("pineJackpots").innerText="du har vunnet 🍍 jackpot: " + pineJackpots + " gang(er)";
+        array = jackpot.concat(pineappleJack);
       }
       if (screen1==="🍌") {
-        bananaJackpots++;
-        document.getElementById("bananaJackpots").innerText="du har vunnet 🍌 jackpot: " + bananaJackpots + " gang(er)";
+        array = jackpot.concat(bananaJack);
       }
-    }
+
+        for (let i = 0; i < 8; i++) {
+        screen2 = array[Math.floor(Math.random() * array.length)];
+        screens.s2.innerText=screen2;
+        await delay(200);
+      }
+
+        for (let i = 0; i < 8; i++) {
+        screen3 = array[Math.floor(Math.random() * array.length)];
+        screens.s3.innerText=screen3;
+        await delay(200);
+      }
+
+      console.log(spinning)
+
+      if (screen1 === screen2 && screen2 === screen3) {
+        console.log(`Du har vunnet ${screen1} jackpot`);
+        document.getElementById("jackpotPrint").innerText=(`Du har vunnet ${screen1} jackpot`);
+
+        if (screen1==="🍋‍🟩") {
+          limeJackpots++;
+          document.getElementById("limeJackpots").innerText="du har vunnet 🍋‍🟩 jackpot: " + limeJackpots + " gang(er)";
+        }
+        if (screen1==="🍉") {
+          melonJackpots++;
+          document.getElementById("melonJackpots").innerText="du har vunnet 🍉 jackpot: " + melonJackpots + " gang(er)";
+        }
+        if (screen1==="🍋") {
+          lemonJackpots++;
+          document.getElementById("lemonJackpots").innerText="du har vunnet 🍋 jackpot: " + lemonJackpots + " gang(er)";
+        }
+        if (screen1==="🍒") {
+          cherryJackpots++;
+          document.getElementById("cherryJackpots").innerText="du har vunnet 🍒 jackpot: " + cherryJackpots + " gang(er)";
+        }
+        if (screen1==="🍇") {
+          grapeJackpots++;
+          document.getElementById("grapeJackpots").innerText="du har vunnet 🍇 jackpot: " + grapeJackpots + " gang(er)";
+        }
+        if (screen1==="🍍") {
+          pineJackpots++;
+          document.getElementById("pineJackpots").innerText="du har vunnet 🍍 jackpot: " + pineJackpots + " gang(er)";
+        }
+        if (screen1==="🍌") {
+          bananaJackpots++;
+          document.getElementById("bananaJackpots").innerText="du har vunnet 🍌 jackpot: " + bananaJackpots + " gang(er)";
+        }
+      }
+      spinning = false;
+      gambleButtonColor.style.backgroundColor = "lightgrey";
+  }
 }
